e48e1ef6fb2d2b382a67e54a053ffdd319018c91
Abhishek Velayudham <avelay@terpmail.umd.edu>
2020-09-24 23:00:25 -0400

    adding access to stitch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Stitch, UserApiKeyCredential } from "mongodb-stitch-browser-sdk";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import Loader from "react-loader-spinner";
+import Listings from "./listings/Listings";
 
 export default function App() {
+  const [client, setClient] = useState(null);
+  useEffect(() => {
+    const stitchAppClient = Stitch.initializeDefaultAppClient(
+      process.env.REACT_APP_MONGO_DB_APP_ID
+    );
+    const credential = new UserApiKeyCredential(
+      process.env.REACT_APP_MONGO_DB_API_KEY
+    );
+    stitchAppClient.auth
+      .loginWithCredential(credential)
+      .then((authedId) => {
+        setClient(stitchAppClient);
+      })
+      .catch((err) => console.error(`login failed with error: ${err}`));
+  }, []);
+
+  if (!client) {
+    return (
+      <Loader
+        className="_webpageLoading"
+        type="ThreeDots"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+    );
+  }
+
   return (
     <Router>
       <div>
@@ -42,9 +74,7 @@ function Home() {
   return (
     <div>
       <h2>Home</h2>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <Listings />
     </div>
   );
 }
---
+++ frontend/src/listings/Listings.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+
+export default function Listings() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+        />
+      </Container>
+    </React.Fragment>
+  );
+}
