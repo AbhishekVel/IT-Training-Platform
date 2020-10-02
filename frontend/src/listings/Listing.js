bbf437ac7dbd68ad20cf13ce695112150663250d
Abhishek Velayudham <avelay@terpmail.umd.edu>
2020-10-02 16:19:07 -0400

    redo listing to use grid instead -- more responsive and remove demo dates

--- frontend/src/listings/Listing.js
+++ frontend/src/listings/Listing.js
@@ -3,175 +3,117 @@ import { makeStyles } from "@material-ui/core/styles";
 import defaultImage from "./defaultcourseimg.jpg"; // Tell webpack this JS file uses this image
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
-import Collapse from "@material-ui/core/Collapse";
-import List from "@material-ui/core/List";
-import Box from "@material-ui/core/Box";
-import { ListItemIcon } from "@material-ui/core";
-import { ListItemText } from "@material-ui/core";
-import EventIcon from "@material-ui/icons/Event";
 import { useMediaQuery } from "react-responsive";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    display: "flex",
-    margin: ".5% .1%",
-    padding: ".25% .1%",
-    flexDirection: "row",
-    width: "100%",
-  },
-  courseDetails: {
-    flexDirection: "row",
-    display: "flex",
-    flex: 1,
-    width: "100%",
-  },
-  leftSideCourseDetails: {
-    display: "flex",
-    flexDirection: "column",
-    flex: 1,
-  },
-  rightSideCourseDetails: {
-    display: "flex",
-    marginLeft: "auto",
-    flexDirection: "column",
-  },
-  courseName: {
-    marginTop: 0,
-    marginBottom: 0,
-    // color: "#444053",
-    fontSize: "1rem",
-    fontFamily: "Palantino",
-  },
-  courseDescription: {
-    marginTop: "2px",
-    marginBottom: 0,
-    fontSize: ".95rem",
-    fontFamily: "Palantino",
-  },
-  courseTeacher: {
-    fontSize: ".9rem",
-    marginTop: "1px",
-    color: "#303030",
-    fontFamily: "Palantino",
-  },
-  companyName: {
-    marginTop: "auto",
-    marginBottom: 0,
-    width: "100%",
-    color: "#303030",
-    fontSize: ".9rem",
-    fontFamily: "Palantino",
-  },
-  courseDemoListings: {
-    flex: "display",
-    flexDirection: "column",
-  },
-  collapsableItem: {
-    paddingTop: "1%",
-    marginTop: "1%",
-    width: "100%",
-    borderTop: "1px solid",
-    borderTopColor: "#E8E8E8",
-  },
-}));
+const useStyles = (onPhone) =>
+  makeStyles((theme) => ({
+    courseName: {
+      fontSize: onPhone ? ".8rem" : "1.1rem",
+      fontFamily: "Palantino",
+      marginBottom: 0,
+      paddingBottom: 0,
+      fontWeight: "bold",
+      color: "#444053",
+    },
+    courseDescription: {
+      fontSize: onPhone ? ".6rem" : ".9rem",
+      fontFamily: "Palantino",
+      marginBottom: 0,
+      paddingBottom: 0,
+    },
+    courseTeacher: {
+      fontSize: onPhone ? ".6rem" : ".9rem",
+      color: "#303030",
+      fontFamily: "Palantino",
+    },
+    companyName: {
+      color: "#303030",
+      fontSize: onPhone ? ".6rem" : ".9rem",
+      fontFamily: "Palantino",
+    },
+    price: {
+      fontSize: onPhone ? ".6rem" : ".9rem",
+      fontFamily: "Palantino",
+      fontWeight: "bold",
+      color: "#444053",
+    },
+    courseDuration: {
+      fontSize: onPhone ? ".6rem" : ".9rem",
+      fontFamily: "Palantino",
+      color: "#505050",
+    },
+    courseImage: {
+      marginTop: onPhone ? "10px" : undefined,
+      width: onPhone ? "38px" : "150px",
+      height: onPhone ? "33px" : "80px",
+      borderRadius: "10px",
+      marginRight: "3%",
+    },
+  }));
 
 export default function Listing({ data, shaded }) {
-  const classes = useStyles();
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
-  const [open, setOpen] = React.useState(false);
-  var dateFormatOptions = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-  };
+  const classes = useStyles(isTabletOrMobile)();
 
   return (
-    <Box boxShadow={open ? 1 : 0}>
-      <ListItem
-        onClick={() => setOpen(!open)}
-        button
-        // This adds a slight tint to every other bar -- just though it'd add a bit of hazazzz
-        style={{
-          backgroundColor: shaded ? "#F7F5F5" : undefined,
-          flexDirection: "column",
-          marginLeft: 0,
-          paddingLeft: 0,
-        }}
-      >
-        <div className={classes.container}>
-          <div
-            style={{
-              maxWidth: "18%",
-              maxHeight: "150px",
-              display: "flex",
-              marginRight: "1rem",
-              flex: 1,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "50% 50%",
-              backgroundImage: `url(${data.courseImageUrl || defaultImage})`,
-            }}
+    <ListItem
+      button
+      style={{
+        flexDirection: "column",
+        padding: "1%",
+        marginBottom: "1%",
+        backgroundColor: shaded ? "#F7F5F5" : undefined,
+      }}
+    >
+      {/* Grid container for image */}
+      <Grid container spacing={0}>
+        <Grid item xs={2}>
+          <img
+            className={classes.courseImage}
+            src={data.courseImageUrl || defaultImage}
           />
-          <div className={classes.courseDetails}>
-            <div className={classes.leftSideCourseDetails}>
-              <b className={classes.courseName}>{data.courseName}</b>
-              <p className={classes.courseDescription}>{data.description}</p>
-              <p className={classes.courseTeacher}>{data.teacherName}</p>
-            </div>
+        </Grid>
+        {/* Grid container for course details */}
+        <Grid item container xs={10}>
+          <Grid item xs={9}>
+            <Typography className={classes.courseName}>
+              {data.courseName}
+            </Typography>
+          </Grid>
+          <Grid item xs={3} container justify="flex-end">
+            <Typography className={classes.price}>₹{data.price}</Typography>
+          </Grid>
+          {/* NEW ROW */}
+          <Grid item xs={12}>
+            <Typography className={classes.courseDescription}>
+              {data.briefDescription}
+            </Typography>
+          </Grid>
 
-            <div className={classes.rightSideCourseDetails}>
-              <p className={classes.companyName}>{data.companyName}</p>
-            </div>
-          </div>
-        </div>
-        <Collapse
-          in={open}
-          timeout="auto"
-          unmountOnExit
-          className={classes.collapsableItem}
-        >
-          <List component="div" disablePadding>
-            <div className={classes.courseDemoListings}>
-              <ListItem>
-                <b
-                  style={{
-                    fontSize: ".9rem",
-                  }}
-                >
-                  Available Course Demos:
-                </b>
-              </ListItem>
-              {data.listings &&
-                data.listings.map((demoInfo, index) => {
-                  return (
-                    <ListItem className={classes.demoDateListItem} key={index}>
-                      <ListItemIcon>
-                        <EventIcon />
-                      </ListItemIcon>
-                      <Link href={demoInfo.link}>
-                        <p
-                          style={{
-                            fontSize: ".9rem",
-                            margin: "0 0",
-                          }}
-                        >
-                          {new Date(demoInfo.date).toLocaleDateString(
-                            "en-us",
-                            dateFormatOptions
-                          )}
-                        </p>
-                      </Link>
-                    </ListItem>
-                  );
-                })}
-            </div>
-          </List>
-        </Collapse>
-      </ListItem>
-    </Box>
+          {/* NEW ROW */}
+          <Grid item xs={5}>
+            <Typography className={classes.courseTeacher}>
+              {data.teacherName}
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            {/* empty space*/}
+          </Grid>
+          <Grid item xs={5} container justify="flex-end">
+            <Typography className={classes.companyName}>
+              {data.companyName}
+            </Typography>
+          </Grid>
+          {/* NEW ROW */}
+
+          <Grid item xs={5}>
+            <Typography className={classes.courseDuration}>
+              {data.totalHours} hours ○ {data.totalDays} days
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </ListItem>
   );
 }
