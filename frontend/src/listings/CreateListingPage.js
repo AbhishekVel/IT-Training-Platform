0be0aaf9d3f7f6263fe8579d3852d5551191d513
javedshaik <jshaik@terpmail.umd.edu>
2020-10-11 19:05:30 -0400

    newly created add listing page

--- frontend/src/listings/CreateListingPage.js
+++ frontend/src/listings/CreateListingPage.js
@@ -11,16 +11,27 @@ import SendIcon from "@material-ui/icons/Send";
 
 const defaultRepeatedValues = [
   {
-    courseName: "",
-    courseDateTime: "",
+    syllabus: "",
   },
 ];
 
 const defaultValues = [
   {
-    firstName: "",
-    lastName: "",
     companyName: "",
+    courseName: "",
+    courseDescription: "",
+    coursePrice: "",
+    courseImageUrl: "",
+    fullCourseDescription: "",
+    courseLink: "",
+    contactPhone: "",
+    contactEmail: "",
+    courseHours: "",
+    courseDays: "",
+    teacherName: "",
+    teacherPicture: "",
+    teacherBio: "",
+    sampleLectureCourseYoutubeUrl: "",
   },
 ];
 
@@ -55,10 +66,14 @@ const useStyles = makeStyles((theme) => ({
   },
   addRemoveButton: {
     textDecoration: "none",
+    textTransform: "capitalize",
+  },
+  submitButton: {
+    marginTop: "15px",
   },
 }));
 
-export default function CreateListing() {
+export default function CreateListing({ user }) {
   const classes = useStyles();
   const [fields, setFields] = useState(defaultValues);
   const [repeatedFields, setRepeatedFields] = useState(defaultRepeatedValues);
@@ -66,7 +81,7 @@ export default function CreateListing() {
   function handleChangeInput(event) {
     const values = [...fields];
     const { name, value } = event.target;
-    values[name] = value;
+    values[0][name] = value;
     setFields(values);
   }
 
@@ -80,8 +95,7 @@ export default function CreateListing() {
   function handleAddInputRepeatedFields() {
     const values = [...repeatedFields];
     values.push({
-      courseName: "",
-      courseDateTime: "",
+      syllabus: "",
     });
     setRepeatedFields(values);
   }
@@ -94,8 +108,31 @@ export default function CreateListing() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(fields);
-    console.log(repeatedFields);
+
+    //send to backend
+    var objToSendToBackend = {
+      companyName: fields[0].companyName,
+      courseName: fields[0].courseName,
+      courseDescription: fields[0].courseDescription,
+      coursePrice: fields[0].coursePrice,
+      courseImageUrl: fields[0].courseImageUrl,
+      fullCourseDescription: fields[0].fullCourseDescription,
+      courseLink: fields[0].courseLink,
+      contactPhone: fields[0].contactPhone,
+      contactEmail: fields[0].contactEmail,
+      courseHours: fields[0].courseHours,
+      courseDays: fields[0].courseDays,
+      teacherName: fields[0].teacherName,
+      teacherPicture: fields[0].teacherPicture,
+      teacherBio: fields[0].teacherBio,
+      sampleLectureCourseYoutubeUrl: fields[0].sampleLectureCourseYoutubeUrl,
+      syllabus: repeatedFields,
+    };
+
+    user.functions.insertListing(objToSendToBackend);
+
+    //clear input
+    e.target.reset();
   }
 
   return (
@@ -110,64 +147,193 @@ export default function CreateListing() {
             gutterBottom
             className={classes.formTitle}
           >
-            Add Listing
+            Add Course
           </Typography>
           <form encType="multipart/form-data" onSubmit={(e) => handleSubmit(e)}>
             <Grid container spacing={3}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
                 <TextField
                   required
-                  id="firstName"
-                  name="firstName"
-                  label="First name"
+                  id="courseName"
+                  name="courseName"
+                  label="Course name"
+                  maxLength={80}
                   fullWidth
-                  value={fields.firstName}
+                  value={fields.courseName}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="courseDescription"
+                  name="courseDescription"
+                  label="Brief Course Overview"
+                  multiline={true}
+                  maxLength={200}
+                  fullWidth
+                  value={fields.courseDescription}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="coursePrice"
+                  name="coursePrice"
+                  label="Course Price"
+                  type="number"
+                  fullWidth
+                  value={fields.coursePrice}
                   onChange={(e) => handleChangeInput(e)}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
                   required
-                  id="lastName"
-                  name="lastName"
-                  label="Last name"
+                  id="courseHours"
+                  name="courseHours"
+                  label="Total length of course (hours)"
+                  type="number"
                   fullWidth
-                  value={fields.lastName}
+                  value={fields.courseHours}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="courseDays"
+                  name="courseDays"
+                  label="Total length of course (days)"
+                  type="number"
+                  fullWidth
+                  value={fields.courseDays}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="courseLink"
+                  name="courseLink"
+                  label="Course Link (optional)"
+                  fullWidth
+                  value={fields.courseLink}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="courseImageUrl"
+                  name="courseImageUrl"
+                  label="Course Image Url (optional)"
+                  fullWidth
+                  value={fields.courseImageUrl}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="fullCourseDescription"
+                  name="fullCourseDescription"
+                  label="Full Course Description"
+                  fullWidth
+                  multiline={true}
+                  value={fields.fullCourseDescription}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="sampleLectureCourseYoutubeUrl"
+                  name="sampleLectureCourseYoutubeUrl"
+                  label="Sample lecture youtube URL (optional)"
+                  fullWidth
+                  multiline={true}
+                  value={fields.sampleLectureCourseYoutubeUrl}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="teacherName"
+                  name="teacherName"
+                  label="Teacher name (optional)"
+                  fullWidth
+                  value={fields.teacherName}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="teacherPicture"
+                  name="teacherPicture"
+                  label="Teacher picture Url (optional)"
+                  fullWidth
+                  value={fields.teacherPicture}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="teacherBio"
+                  name="teacherBio"
+                  label="Teacher bio (optional)"
+                  fullWidth
+                  value={fields.teacherBio}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="companyName"
+                  name="companyName"
+                  label="Company name (optional)"
+                  fullWidth
+                  value={fields.companyName}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="contactEmail"
+                  name="contactEmail"
+                  label="Company contact email (optional)"
+                  fullWidth
+                  value={fields.contactEmail}
+                  onChange={(e) => handleChangeInput(e)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="contactPhone"
+                  name="contactPhone"
+                  label="Company contact phone (optional)"
+                  type="number"
+                  fullWidth
+                  value={fields.contactPhone}
                   onChange={(e) => handleChangeInput(e)}
                 />
               </Grid>
             </Grid>
             {repeatedFields.map((field, idx) => {
               return (
-                <div>
+                <Grid>
                   <Grid>
-                    <Grid xs={12} key={`${field}-${idx}`}>
+                    <Grid item xs={12} key={`${field}-${idx}`}>
                       <TextField
                         required
-                        id="courseName"
-                        name="courseName"
-                        label="Course Name"
+                        id="syllabus"
+                        name="syllabus"
+                        label="Syllabus [Course topic ex. Hadoop, Java]"
                         fullWidth
-                        value={repeatedFields.courseName}
+                        value={repeatedFields.syllabus}
                         onChange={(e) =>
                           handleChangeInputRepeatedFields(idx, e)
                         }
                       />
                     </Grid>
-                    <TextField
-                      required
-                      id="courseDateTime"
-                      name="courseDateTime"
-                      label="Date and Time"
-                      type="datetime-local"
-                      fullWidth
-                      autoComplete="shipping address-line2"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                      value={repeatedFields.courseDateTime}
-                      onChange={(e) => handleChangeInputRepeatedFields(idx, e)}
-                    />
                   </Grid>
                   <Grid
                     container
@@ -176,38 +342,27 @@ export default function CreateListing() {
                     alignItems="flex-end"
                   >
                     <Button
-                      size="large"
+                      className={classes.addRemoveButton}
                       onClick={() => handleAddInputRepeatedFields()}
                       startIcon={<AddBoxIcon />}
                     >
-                      Add course
+                      Add course topic
                     </Button>
                     <Button
                       className={classes.addRemoveButton}
-                      size="large"
                       startIcon={<DeleteForeverIcon />}
                       onClick={() => handleRemoveInputRepeatedFields(idx)}
                     >
-                      Remove course
+                      delete course topic
                     </Button>
                   </Grid>
-                </div>
+                </Grid>
               );
             })}
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                <TextField
-                  required
-                  id="companyName"
-                  name="companyName"
-                  label="Company name"
-                  fullWidth
-                  value={fields.companyName}
-                  onChange={(e) => handleChangeInput(e)}
-                />
-              </Grid>
-              <Grid item xs={12}>
                 <Button
+                  className={classes.submitButton}
                   startIcon={<SendIcon />}
                   variant="contained"
                   color="primary"
